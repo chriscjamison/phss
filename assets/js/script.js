097dc78a4975b2f257de3a202027e8e2ddd8b1f4
Chris Jamison <chris@chriscjamison.com>
2018-05-14 23:11:21 -0700

    0.14 - Base HTML, CSS, and Animations complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -94,31 +94,183 @@ $(window).scroll(
     nav_selector = "nav";
     nav_element = $(nav_selector);
 
+    var ul_selector;
+    var ul_element = {};
+
+    ul_selector = "nav ul";
+    ul_element = $(ul_selector);
+
     var nav_css = {};
+    var ul_css = {};
 
-    if (window_position >= 78 && 
-        (Math.ceil(body_height) - window_position > window_height))  {
+    var nav_height;
 
+    nav_height = $("nav").height();
+
+    var nav_top_value;
+
+    nav_top_value = (window_height - nav_height - 2).toString() + "px";
+/* console.log("nav_height = " + nav_height);
+console.log("nav_top_value = " + nav_top_value); */
+    var window_at_bottom_value;
+
+    window_at_bottom_value = window_position - (Math.round(body_height) - window_height);
+// console.log("window_at_bottom_value = " + window_at_bottom_value);
+    if (window_position >= 78) {
       nav_css = {
-        top: "0px", 
         position: "fixed"
       };
 
-      $(nav_element).css(nav_css);
-    } else if (window_position < 78)  {
-      nav_css = {
-        position: "relative"
-      }
+/*        */
 
-      $(nav_element).css(nav_css);
-    } else if (Math.ceil(body_height) - window_position <= window_height) {
-// console.log("Math.ceil(" + (body_height).toString() + ") - " + (window_position).toString() + " = " + (Math.ceil(body_height) - window_position));
-      nav_css = {
-        top: (body_height - $(nav_element).height()) + "px", 
-        position: "relative"
-      }
+      if (window_at_bottom_value === 56 || 
+          window_at_bottom_value === -4) {
+        nav_css.top = nav_top_value;
 
+        if ($(nav_element).css("top") === "0px" )  {
+          
+          $(nav_element).stop().hide("slide", { direction: "up" }, 750, 
+            function () {  
+              $(nav_element).css(nav_css);
+console.log("1");
+              $(nav_element).stop().show("slide", { direction: "down" }, 500);
+            }
+          ); 
+
+          // $(nav_element).stop().show("slide", { direction: "down" }, 500);
+        } else {
+          $(nav_element).css(nav_css);
+
+          $(nav_element).stop().show("slide", { direction: "down" }, 500);
+          /* $(nav_element).stop().hide("slide", { direction: "up", distance: nav_height }, 750, 
+            function () {
+              $(nav_element).css(nav_css);
+
+              $(nav_element).stop().show("slide", { direction: "down" }, 500);
+            }
+          ); */
+console.log("2");
+        }
+      } else {
+// console.log("$(window).scrollTop() = " + $(window).scrollTop());
+        nav_css.top = "0px";
+// console.log("3");
+        if (window_position < body_height - (window_height * 1.4))  {
+          if ($(nav_element).css("top") !== "0px")  {
+            // $(nav_element).css(nav_css);
+
+            $(nav_element).stop().hide("slide", { direction: "down" }, 500, 
+              function () {
+                $(nav_element).css(nav_css);
+
+                $(nav_element).stop().show("slide", { direction: "up" }, 500);
+              });
+console.log("0px");
+          }
+  
+          if ($(nav_element).css("position") === "relative")  {
+            $(nav_element).css(nav_css);
+console.log("relative");
+          }
+        }
+      }
+/* console.log("window_position = " + window_position);
+      if (window_position < 6304 - (window_height / 2))  {
+        if ($(nav_element).css("top") !== nav_top_value && 
+            $(nav_element).css("position") === "relative") {
+          $(nav_element).css(nav_css);
+        } else {
+          if ($(nav_element).css("top") === nav_top_value)  {
+            $(nav_element).stop().hide("slide", { direction: "down" }, 500, 
+              function () {
+  
+  console.log("bottom up");
+                /* $(nav_element).css(nav_css);
+  
+  
+  
+                $(nav_element).stop().show("slide", { direction: "down" }, 500); 
+              }
+            );
+          } else {
+            $(nav_element).hide("slide", { direction: "up", distance: nav_height }, 750, 
+              function () {
+                nav_css.top = nav_top_value;
+    console.log("going down");
+                $(nav_element).css(nav_css);
+                $(nav_element).stop().show("slide", { direction: "down" }, 500);
+              }
+            );
+          }
+        }
+      } 
+ */ } else  {
+      nav_css = {
+        top: "0px", 
+        display: "flex", 
+        position: "relative"
+      };
+
+      ul_css = {
+        marginTop: "0"
+      };
+
+      $(ul_element).css(ul_css);
       $(nav_element).css(nav_css);
+    } 
+  }
+);
+
+/* $("a#nav-link-phss_supporters").click(
+  function () {
+    var window_height;
+
+    window_height = $(window).height();
+
+    var nav_height;
+
+    nav_height = $("nav").height();
+
+    nav_top_value = (window_height - nav_height - 4).toString() + "px";
+
+    var nav_selector;
+    var nav_element = {};
+
+    nav_selector = "nav";
+    nav_element = $(nav_selector);
+
+    var nav_css = {};
+
+    nav_css = {
+      top: nav_top_value, 
+      position: "fixed", 
+      borderTop: "2px solid #ec9053"
+    };
+
+    $(nav_element).css(nav_css);
+  }
+);
+ */
+$("nav > ul:nth-child(2) > li > a").click(
+  function () {
+    var window_position;
+
+    window_position = $(window).scrollTop();
+
+    if (window_position === 0)  {
+      var link_id_value;
+
+      link_id_value = $(this).attr("id");
+
+      if (link_id_value !== "nav-link-phss_supporters") {
+        setTimeout(
+          function () {
+            window_position = $(window).scrollTop();
+  
+            $(window).scrollTop(window_position - 60);
+          }, 100
+        );
+      }
     }
   }
 );
